780fdad1587bd256df67835488dcf84f1dcd408d
Dane Laban <TheZepto@users.noreply.github.com>
2017-09-29 11:15:12 +1000

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -5,12 +5,53 @@ require 'scraperwiki'
 require 'mechanize'
 require 'date'
 
-periods = [2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007]
-periods.map(&:to_s).product([*'-01'..'-12'].reverse).map(&:join).select{|d| d <= Date.today.to_s[0..-3]}
+def scrape_page(page, comment_url)
+  page.at("table.rgMasterTable").search("tr.rgRow,tr.rgAltRow").each do |tr|
+    tds = tr.search('td').map{|t| t.inner_html.gsub("\r\n", "").strip}
+    day, month, year = tds[2].split("/").map{|s| s.to_i}
+    record = {
+      "info_url" => (page.uri + tr.search('td').at('a')["href"]).to_s,
+      "council_reference" => tds[1],
+      "date_received" => Date.new(year, month, day).to_s,
+      "description" => tds[3].gsub("&amp;", "&").split("<br>")[1].to_s.squeeze(" ").strip,
+      "address" => tds[3].gsub("&amp;", "&").split("<br>")[0].gsub("\r", " ").gsub("<strong>","").gsub("</strong>","").squeeze(" ").strip,
+      "date_scraped" => Date.today.to_s,
+      "comment_url" => comment_url
+    }
+    if (ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true)
+      puts "Saving record " + record['council_reference'] + " - " + record['address']
+#      puts record
+      ScraperWiki.save_sqlite(['council_reference'], record)
+    else
+      puts "Skipping already saved record " + record['council_reference']
+    end
+  end
+end
 
-periods.each{ |period| 
+# Implement a click on a link that understands stupid asp.net doPostBack
+def click(page, doc)
+  js = doc["href"] || doc["onclick"]
+  if js =~ /javascript:__doPostBack\('(.*)','(.*)'\)/
+    event_target = $1
+    event_argument = $2
+    form = page.form_with(id: "aspnetForm")
+    form["__EVENTTARGET"] = event_target
+    form["__EVENTARGUMENT"] = event_argument
+    form.submit
+  elsif js =~ /return false;__doPostBack\('(.*)','(.*)'\)/
+    nil
+  else
+    # TODO Just follow the link likes it's a normal link
+    raise
+  end
+end
+
+periodstrs = [2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007]
+periodstrs.map(&:to_s).product([*'-01'..'-12'].reverse).map(&:join).select{|d| d <= Date.today.to_s[0..-3]}
+
+periodstrs.each{ |periodstr| 
   
-  ENV['MORPH_PERIOD'] = period 
+  ENV['MORPH_PERIOD'] = periodstr 
 
   # Scraping from Masterview 2.0
   case ENV['MORPH_PERIOD']
@@ -34,47 +75,6 @@ periods.each{ |period|
       end
   end
   puts "Getting data in `" + ENV['MORPH_PERIOD'] + "`, changable via MORPH_PERIOD environment"
-
-  def scrape_page(page, comment_url)
-    page.at("table.rgMasterTable").search("tr.rgRow,tr.rgAltRow").each do |tr|
-      tds = tr.search('td').map{|t| t.inner_html.gsub("\r\n", "").strip}
-      day, month, year = tds[2].split("/").map{|s| s.to_i}
-      record = {
-        "info_url" => (page.uri + tr.search('td').at('a')["href"]).to_s,
-        "council_reference" => tds[1],
-        "date_received" => Date.new(year, month, day).to_s,
-        "description" => tds[3].gsub("&amp;", "&").split("<br>")[1].to_s.squeeze(" ").strip,
-        "address" => tds[3].gsub("&amp;", "&").split("<br>")[0].gsub("\r", " ").gsub("<strong>","").gsub("</strong>","").squeeze(" ").strip,
-        "date_scraped" => Date.today.to_s,
-        "comment_url" => comment_url
-      }
-      if (ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true)
-        puts "Saving record " + record['council_reference'] + " - " + record['address']
-  #      puts record
-        ScraperWiki.save_sqlite(['council_reference'], record)
-      else
-        puts "Skipping already saved record " + record['council_reference']
-      end
-    end
-  end
-
-  # Implement a click on a link that understands stupid asp.net doPostBack
-  def click(page, doc)
-    js = doc["href"] || doc["onclick"]
-    if js =~ /javascript:__doPostBack\('(.*)','(.*)'\)/
-      event_target = $1
-      event_argument = $2
-      form = page.form_with(id: "aspnetForm")
-      form["__EVENTTARGET"] = event_target
-      form["__EVENTARGUMENT"] = event_argument
-      form.submit
-    elsif js =~ /return false;__doPostBack\('(.*)','(.*)'\)/
-      nil
-    else
-      # TODO Just follow the link likes it's a normal link
-      raise
-    end
-  end
 
   url = "http://pdonline.logan.qld.gov.au/MasterViewUI/Modules/ApplicationMaster/default.aspx?page=found" + period + "&4a=&6=F"
   comment_url = "mailto:council@logan.qld.gov.au"
